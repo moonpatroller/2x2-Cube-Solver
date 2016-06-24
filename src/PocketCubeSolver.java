15eac5bc7e6f81b6c39a70efa389ec307bdfb8f5
HaginCodes <hagincubes@gmail.com>
2016-06-24 00:02:01 -0400

    going to finish turns then start working on optimial solution. Yaygit add .

--- src/PocketCubeSolver.java
+++ src/PocketCubeSolver.java
@@ -27,6 +27,7 @@ public class PocketCubeSolver {
 		
 		}
 	public static void turn_CC(int cube[][], int side_num){
+		
 			  int cube0_0 = cube[side_num][0];
 		cube[side_num][0] = cube[side_num][1];
 		cube[side_num][1] = cube[side_num][3];
@@ -34,9 +35,9 @@ public class PocketCubeSolver {
 		cube[side_num][2] = cube0_0;
 	}
 	public static void turn_C2(int cube[][], int side_num){
+		
 			  int cube0_0 = cube[side_num][0];
-			  int cube0_1 = cube[side_num][2];
-			  
+			  int cube0_1 = cube[side_num][2];  
 		cube[side_num][0] = cube[side_num][1];
 		cube[side_num][2] = cube[side_num][3];
 		cube[side_num][1] = cube0_1;
@@ -58,7 +59,7 @@ public class PocketCubeSolver {
 		
 		cube[1][0] = cube2_0;
 		cube[1][1] = cube2_1;
-		System.out.println("turn the top side clockwise");
+		System.out.println("U ");
 		}
 	 static void turn_F(int cube[][]){
 		 turn_CW(cube, 2);
@@ -75,7 +76,7 @@ public class PocketCubeSolver {
 		 
 		 cube[3][2] = cube0_3;
 		 cube[3][0] = cube0_2;	 
-		System.out.println("turn the front side clockwise");
+		System.out.println("F ");
 	 }
 	 
 	 static void turn_R(int cube[][]){
@@ -94,7 +95,7 @@ public class PocketCubeSolver {
 		 
 		 cube[4][2] = cube0_1;
 		 cube[4][0] = cube0_3;
-		 System.out.println("turn the right side clockwise");
+		 System.out.println("R ");
 	 }
 	 static void turn_L(int cube[][]){
 		 turn_CW(cube,1);
@@ -112,7 +113,7 @@ public class PocketCubeSolver {
 		 
 		 cube[2][2] = cube0_2;
 		 cube[2][0] = cube0_0;
-		 System.out.println("turn the left side clockwise");
+		 System.out.println("L ");
 	 }
 	 static void turn_B(int cube[][]){
 		 turn_CW(cube,4);
@@ -130,7 +131,7 @@ public class PocketCubeSolver {
 		 
 		 cube[1][2] = cube0_0;
 		 cube[1][0] = cube0_1;
-		 System.out.println("turn the back side clockwise");
+		 System.out.println("B ");
 	 }
 	 
 	 static void turn_D(int cube[][]){
@@ -149,7 +150,7 @@ public class PocketCubeSolver {
 		 
 		 cube[3][2] = cube2_2;
 		 cube[3][3] = cube2_3;
-		 System.out.println("turn the down side clockwise");
+		 System.out.println("D ");
 		 }
 	 
 	 static void turn_Ui(int cube[][]){
@@ -168,7 +169,7 @@ public class PocketCubeSolver {
 		 
 		 cube[3][0] = cube2_0;
 		 cube[3][1] = cube2_2;
-		 System.out.println("turn the up side counter clockwise");
+		 System.out.println("Ui ");
 		 }
 	 static void turn_Fi(int cube[][]){
 		 turn_CC(cube,2);
@@ -186,7 +187,7 @@ public class PocketCubeSolver {
 		 
 		 cube[1][3] = cube0_2;
 		 cube[1][1] = cube0_3;
-		 System.out.println("turn the front side counter clockwise");
+		 System.out.println("Fi ");
  	 }
 	static void turn_Ri(int cube[][]){
 		turn_CC(cube, 3);
@@ -204,7 +205,7 @@ public class PocketCubeSolver {
 		
 		cube[2][1] = cube0_1;
 		cube[2][3] = cube0_3;
-		System.out.println("turn the right side counter clockwise");
+		System.out.println("Ri ");
 		
 	}
 	static void turn_Li(int cube[][]){
@@ -223,7 +224,7 @@ public class PocketCubeSolver {
 		
 		cube[4][1] = cube0_1;
 		cube[4][3] = cube0_2;
-		System.out.println("turn the left side counter clockwise");
+		System.out.println("Li ");
 	}
 	static void turn_Di(int cube[][]){
 		turn_CC(cube, 5);
@@ -241,7 +242,7 @@ public class PocketCubeSolver {
 		
 		cube[1][2] = cube2_2;
 		cube[1][3] = cube2_3;
-		System.out.println("turn the down side counter clockwise");
+		System.out.println("Di");
 	}
 	static void turn_Bi(int cube[][]){
 		turn_CC(cube,4);
@@ -259,7 +260,7 @@ public class PocketCubeSolver {
 		
 		cube[3][3] = cube0_1;
 		cube[3][1] = cube0_0;
-		System.out.println("turn the back side counter clockwise");
+		System.out.println("Bi ");
 	}
 	static void turn_U2(int cube[][]){
 		turn_C2(cube,0);
@@ -280,10 +281,49 @@ public class PocketCubeSolver {
 		
 		cube[4][0] = cube2_0;
 		cube[4][1] = cube2_1;
-		System.out.println("turn the top side left or right twice");	
+		System.out.println("U2 ");	
+	}
+	static void turn_R2(int cube[][]){
+		turn_C2(cube,3);
+		int cube0_1 = cube[0][1];
+		int cube0_3 = cube[0][3];
+		int cube2_1 = cube[2][1];
+		int cube2_3 = cube[2][3];
+		
+		cube[0][1] = cube[5][1];
+		cube[0][3] = cube[5][3];
+		
+		cube[2][1] = cube[4][2];
+		cube[2][3] = cube[4][0];
+		
+		cube[4][2] = cube2_1;
+		cube[4][0] = cube2_3;
+		
+		cube[5][3] = cube0_3;
+		cube[5][1] = cube0_1;
+		System.out.println("R2 ");	
+	}
+	static void turn_F2(int cube[][]){
+		turn_C2(cube,2);
+		int cube0_2 = cube[0][2];
+		int cube0_3 = cube[0][3];
+		int cube1_1 = cube[1][1];
+		int cube1_3 = cube[1][3];
+		
+		cube[0][2] = cube[5][1];
+		cube[0][3] = cube[5][0];
+		
+		cube[1][1] = cube[3][2];
+		cube[1][3] = cube[3][0];
+		
+		cube[3][2] = cube1_1;
+		cube[3][0] = cube1_3;
+		
+		cube[5][1] = cube0_2;
+		cube[5][0] = cube0_3;
+		System.out.println("F2 ");
 	}
 	
-	 
 	static void print_cube(int[][] cube) {
 		print_sides(cube, 0, 1);
 		print_sides(cube, 1, 4);
