733793cf30d5f69696e9472ec206137b07bbc28b
HaginCodes <hagincubes@gmail.com>
2016-07-12 13:26:03 -0400

    forgot to save the completed code in last commit but now all are up to date, GUI works, not yet intergrated into solver, liked coding this part :D

--- src/gui/GUI_input.java
+++ src/gui/GUI_input.java
@@ -81,7 +81,7 @@ public class GUI_input {
 			public void actionPerformed(ActionEvent e){
 				
 				//Execute when FIRST button is pressed	
-				System.out.println("You clicked the button0");
+				System.out.println("You clicked on button0");
 
 				//NOTE: this changes that occur when you click it multiple times
 				if(side[0] == 5){
@@ -117,6 +117,156 @@ public class GUI_input {
 				
 			}
 		});
-	}
+		button1.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent e){
+				
+				System.out.println("You clicked on button1");
+				if(side[1] == 5){
+					side[1] = 0;
+					button1.setBackground(Color.WHITE);
+					cube[Side][1] = 0;
+				}
+				else if(side[1] == 0){
+					side[1] = 1;
+					button1.setBackground(Color.ORANGE);
+					cube[Side][1] = 1;
+				}
+				else if(side[1] == 1){
+					side[1] = 2;
+					button1.setBackground(Color.GREEN);
+					cube[Side][1] = 2;
+				}
+				else if(side[1] == 2){
+					side[1] = 3;
+					button1.setBackground(Color.RED);
+					cube[Side][1] = 3;
+				}
+				else if(side[1] == 3){
+					side[1] = 4;
+					button1.setBackground(Color.BLUE);
+					cube[Side][1] = 4;
+				}
+				else if(side[1] == 4){
+					side[1] = 5;
+					button1.setBackground(Color.YELLOW);
+					cube[Side][1] = 5;
+				}	
+			}
+		});
+		button2.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent e){
+				
+				System.out.println("You clicked on button2");
+				if(side[2] == 5){
+					side[2] = 0;
+					button2.setBackground(Color.WHITE);
+					cube[Side][2] = 0;
+				}
+				else if(side[2] == 0){
+					side[2] = 1;
+					button2.setBackground(Color.ORANGE);
+					cube[Side][0] = 1;
+				}
+				else if(side[2] == 1){
+					side[2] = 2;
+					button2.setBackground(Color.GREEN);
+					cube[Side][2] = 2;
+				}
+				else if(side[2] == 2){
+					side[2] = 3;
+					button2.setBackground(Color.RED);
+					cube[Side][2] = 3;
+				}
+				else if(side[2] == 3){
+					side[2] = 4;
+					button2.setBackground(Color.BLUE);
+					cube[Side][2] = 4;
+				}
+				else if(side[2] == 4){
+					side[2] = 5;
+					button2.setBackground(Color.YELLOW);
+					cube[Side][2] = 5;
+				}
+	
+			}
+				
+		});
+		button3.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent e){
+				if(side[3] == 5){
+					side[3] = 0;
+					button3.setBackground(Color.WHITE);
+					cube[Side][3] = 0;
+				}
+				else if(side[3] == 0){
+					side[3] = 1;
+					button3.setBackground(Color.ORANGE);
+					cube[Side][3] = 1;
+				}
+				else if(side[3] == 1){
+					side[3] = 2;
+					button3.setBackground(Color.GREEN);
+					cube[Side][3] = 2;
+				}
+				else if(side[3] == 2){
+					side[3] = 3;
+					button3.setBackground(Color.RED);
+					cube[Side][3] = 3;
+				}
+				else if(side[3] == 3){
+					side[3] = 4;
+					button3.setBackground(Color.BLUE);
+					cube[Side][3] = 4;
+				}
+				else if(side[3] == 4){
+					side[3] = 5;
+					button3.setBackground(Color.YELLOW);
+					cube[Side][3] = 5;
+				}
 
+			}
+		});
+		next.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent e){
+				
+				if(Side == 0){
+					sideLabel.setText("The top side");
+					Side++;
+				}
+				else if(Side == 1){
+					sideLabel.setText("The left side");
+					Side++;
+				}
+				else if(Side == 2){
+					sideLabel.setText("The front side");
+					Side++;
+				}
+				else if(Side == 3){
+					sideLabel.setText("The right side");
+					Side++;
+				}
+				else if(Side == 4){
+					sideLabel.setText("The back side");
+					Side++;
+				}
+				else if(Side == 5){
+					sideLabel.setText("The bottom side");
+					Side++;
+				}
+				System.out.println(Side);
+			}
+		});
+		done.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent e){
+				
+				System.out.println("Complete");
+			}
+		});
+		
+	}
 }
